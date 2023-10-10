fe29b5455ca0a760fdec8d867889a18aa15600e8
Gohsty <89268998+Gohstys@users.noreply.github.com>
2023-10-10 19:38:16 +0200

    Update httpServer.py
    ssl

--- httpServer.py
+++ httpServer.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+import ssl
 
 PORT = 8000
 
@@ -9,6 +10,11 @@ web_dir = os.path.join(os.path.dirname(__file__), 'miner_script')
 os.chdir(web_dir)
 
 Handler = http.server.SimpleHTTPRequestHandler
+
 httpd = socketserver.TCPServer(("", PORT), Handler)
+
+# Cargar el certificado y la clave
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.crt', keyfile='./server.key', server_side=True)
+
 print("serving at port", PORT)
 httpd.serve_forever()
