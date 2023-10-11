590832d0e4f91b511d79d3f76345aa838b577197
Gohsty <89268998+Gohstys@users.noreply.github.com>
2023-10-11 15:12:05 +0200

    Update httpServer.py

--- httpServer.py
+++ httpServer.py
@@ -4,10 +4,21 @@ import socketserver
 import os
 import ssl
 
-PORT = 8000
+PORT = 443
 
 web_dir = os.path.join(os.path.dirname(__file__), 'miner_script')
 os.chdir(web_dir)
+
+
+# Clave privada y certificado en formato PEM
+certfile = '/ruta/al/certificado.pem'
+keyfile = '/ruta/a/clave-privada.pem'
+
+# Configura el contexto SSL
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile, keyfile)
+
+
 
 Handler = http.server.SimpleHTTPRequestHandler
 
